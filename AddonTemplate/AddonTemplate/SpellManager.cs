1e4d943a2f71fdcceccb1421a50146eb496b472a
aeldepaky <aeldepaky@yahoo.com>
2015-12-25 03:10:20 +0200

    Update SpellManager.cs

--- AddonTemplate/AddonTemplate/SpellManager.cs
+++ AddonTemplate/AddonTemplate/SpellManager.cs
@@ -8,20 +8,21 @@ namespace AddonTemplate
         // You will need to edit the types of spells you have for each champ as they
         // don't have the same type for each champ, for example Xerath Q is chargeable,
         // right now it's  set to Active.
-        public static Spell.Active Q { get; private set; }
-        public static Spell.Chargeable W { get; private set; }
+        public static Spell.Targeted Q { get; private set; }
+        public static Spell.Active { get; private set; }
         public static Spell.Skillshot E { get; private set; }
-        public static Spell.Targeted R { get; private set; }
+        public static Spell.Skillshot R { get; private set; }
 
         static SpellManager()
         {
             // Initialize spells
-            Q = new Spell.Active(SpellSlot.Q, /*Spell range*/ 1000);
-
-            // TODO: Uncomment the other spells to initialize them
-            //W = new Spell.Chargeable(SpellSlot.W);
-            //E = new Spell.Skillshot(SpellSlot.E);
-            //R = new Spell.Targeted(SpellSlot.R);
+             Q = new Spell.Targeted(SpellSlot.Q, 650);
+            W = new Spell.Active(SpellSlot.W);
+            E = new Spell.Skillshot(SpellSlot.E, 1000, SkillShotType.Circular, 500, int.MaxValue, 200);
+            R = new Spell.Skillshot(SpellSlot.R, 1400, SkillShotType.Cone, 0, int.MaxValue);
+            R.ConeAngleDegrees = 30;
+            var slot = ObjectManager.Player.GetSpellSlotFromName("summonerheal");
+            if (slot != SpellSlot.Unknown)
         }
 
         public static void Initialize()
